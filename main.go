3051e1211ef76522e22b356a051d33d237eb0c29
Assad Ginem <assad@Assads-MacBook-Pro.local>
2025-12-15 10:49:40 -0600

    learning

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+import "fmt"
+func main(){
+
+Name := "assad"
+Age := 18
+race := "Muslim"
+isTall := true
+hasbigfeet := true
+isgay := false
+canplaysoccer := true
+howtall := 68.46
+
+fmt.Printf(
+	"Name: %s\n Age: %d\n race: %s\n is Tall: %t\n has big feet: %t\n is gay: %t\n can play soccer: %t\n how tall: %f\n",
+	Name, Age, race, isTall, hasbigfeet, isgay, canplaysoccer, howtall,
+)
+
+
+
+}
+
+
+
+
+
+
+
+
+	
+
+
+	
+
+
+
+
