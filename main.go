c763b875c7afe69419747fe66e7830395125482d
Assad Ginem <assad@Assads-MacBook-Pro.local>
2025-12-16 14:50:51 -0600

    Varible Learning

--- main.go
+++ main.go
@@ -1,19 +1,26 @@
 package main
 import "fmt"
 func main(){
-x := 582
-y := 392
 
-fmt.Printf("Addition : %d + %d = %d\n", x, y, x+y)
-fmt.Printf("Subtraction : %d - %d = %d\n", x, y, x-y)
-fmt.Printf("Multipication : %d * %d = %d\n", x, y, x*y)
-fmt.Printf("Division : %d / %d = %d\n", x, y, x/y)
-fmt.Printf("Remainder : %d %% %d = %d\n", x, y, x%y)
 
+var myvariable1, myvariable2, myvariable3 = 2, "GFG", 67.56
+
+fmt.Printf("The value of myvariable1 is : %d\n", myvariable1)
+
+fmt.Printf("The type of myvariable1 is : %T\n", myvariable1)
+
+fmt.Printf("\nThe value of myvariable2 is : %s\n",myvariable2)
+
+fmt.Printf("The type of myvariable2 is : %T\n",   myvariable2)
+
+fmt.Printf("\nThe value of myvariable3 is : %f\n", myvariable3)
+
+fmt.Printf("The type of myvariable3 is : %T\n",  myvariable3)
+}
 	
 
 
-}
+
 
 
 
