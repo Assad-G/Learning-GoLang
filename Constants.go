960a12c1930613834ac30bccd74f4097df6f9137
Assad Ginem <assad@Assads-MacBook-Pro.local>
2025-12-16 16:22:45 -0600

    Constants

---
+++ Constants.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+func main(){
+	Constants()
+}
+func Constants(){
+const A = "Assad" 
+const ayr = "Are Your Ready?"
+const pi = 3.14
+const T = true
+const b = "GLHF"
+const c = "Good Luck Have Fun"
+
+fmt.Println("Sup", A)
+fmt.Println("So", ayr)
+fmt.Println( A, ayr, pi)
+fmt.Println("Are you Smart?", T )
+fmt.Println(b>c)
+
+}
