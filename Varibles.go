60874d854e9bd992c4f0780090083ae9db51441c
Assad Ginem <assad@Assads-MacBook-Pro.local>
2025-12-16 15:52:48 -0600

    Varibles

---
+++ Varibles.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main(){
+    Varibles()
+}
+func Varibles(){
+    // Your variables
+    Name := "Assad"
+    Age := 18
+    Race := "Muslim"
+    IsTall := true
+    HasBigFeet := true
+    IsGay := false
+    CanPlaySoccer := true
+    HowTall := 68.46 // inches
+
+    // Print them
+    fmt.Println("Name:", Name)
+    fmt.Println("Age:", Age)
+    fmt.Println("Race:", Race)
+    fmt.Println("Is Tall?", IsTall)
+    fmt.Println("Has Big Feet?", HasBigFeet)
+    fmt.Println("Is Gay?", IsGay)
+    fmt.Println("Can Play Soccer?", CanPlaySoccer)
+    fmt.Println("Height in inches:", HowTall)
+
+
+
+}
+
+
+
