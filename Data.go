4016cd0a32ea1405e4e795a0d4b3404443111944
Assad Ginem <assad@Assads-MacBook-Pro.local>
2025-12-16 15:07:17 -0600

    Add main.go and Data.go with variables and math functions

---
+++ Data.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+    Data()
+}
+
+func Data() {
+    var x int16 = 170
+    var y int16 = 83
+    //Addition
+    fmt.Printf(" addition :  %d + %d = %d\n ", x, y, x+y)
+    //Subtraction
+    fmt.Printf("subtraction : %d - %d = %d\n", x, y, x-y)
+    //Multiplication
+    fmt.Printf(" multiplication : %d * %d = %d\n", x, y, x*y)
+    //Division
+    fmt.Printf(" division : %d / %d = %d\n", x, y, x/y)
+    //Modulus
+    fmt.Printf(" remainder : %d %% %d = %d\n", x, y, x%y)
+}
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+	
+
+
+
+
